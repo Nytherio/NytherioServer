b9ee3ce6dc2c9b92a407348f8bc0f645be98b3f7
Jesse Harrington <Jesse Harrington>
2017-02-25 23:01:41 -0500

    Initial commit

---
+++ ClassLibrary/VehicleSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace TestServer
+{
+    public class VehicleSpawner : Script
+
+    {
+        [Command("vehicle")]
+        public void VehicleSpawn(Client sender, VehicleHash vehicle)
+        {
+             Vehicle spawnedVehicle = API.createVehicle(vehicle, sender.position, sender.rotation, 0, 0);
+            API.setPlayerIntoVehicle(sender, spawnedVehicle, -1);
+            API.setVehicleEngineStatus(spawnedVehicle, true);
+        }
+    }
+}
